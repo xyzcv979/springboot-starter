e16d7ba25d7edf5142e34ad2c07f0c00c279c054
Alex Ng <avon1462@gmail.com>
2022-06-11 17:09:35 -0700

    Working spring boot app

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # springboot-starter
+
+### Tutorial links:
+
+[spring.io](https://spring.io/guides/gs/spring-boot/)
+
+[tutorialspoint](https://www.tutorialspoint.com/spring_boot/index.htm)
+
+### Description:
+Project to learn Spring Boot, web services, and REST APIs
---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Does /test work??";
+    }
+}
